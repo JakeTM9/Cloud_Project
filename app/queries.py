0360ea36d9abc97fd2fa8e41cdce1f25cea4ec00
JakeTM9 <wardegjn@mail.uc.edu>
2021-12-04 16:55:42 -0500

    Sort order, added all necessary columns, fixed os.path.join(), fixed requirements.txt

--- app/queries.py
+++ app/queries.py
@@ -28,10 +28,23 @@ def standardDatapull(household):
     AllProductsDF = DataFrame(list_allProducts)
 
     ##FORMAT HOUSEHOLD AND TRANSACTION DFS
-    houseHoldDF = houseHoldDF[["HSHD_NUM"]]
-    transactionDF = transactionDF[["BASKET_NUM", "PURCHASE_","PRODUCT_NUM"]]
-    finalDF = pd.concat([houseHoldDF, transactionDF], axis = 1)
+    #houseHoldDF = houseHoldDF[["HSHD_NUM"]]
+    #transactionDF = transactionDF[["BASKET_NUM", "PURCHASE_","PRODUCT_NUM"]]
+    transactionDF = transactionDF.drop('HSHD_NUM', axis=1)
+    finalDF = pd.concat([transactionDF, houseHoldDF ], axis = 1)
     finalDF["HSHD_NUM"] = finalDF["HSHD_NUM"][0]
+    finalDF["L"] = finalDF["L"][0]
+    finalDF["AGE_RANGE"] = finalDF["AGE_RANGE"][0]
+    finalDF["MARITAL"] = finalDF["MARITAL"][0]
+    finalDF["INCOME_RANGE"] = finalDF["INCOME_RANGE"][0]
+    finalDF["HOMEOWNER"] = finalDF["HOMEOWNER"][0]
+    finalDF["HSHD_COMPOSITION"] = finalDF["HSHD_COMPOSITION"][0]
+    finalDF["HH_SIZE"] = finalDF["HH_SIZE"][0]
+    finalDF["CHILDREN"] = finalDF["CHILDREN"][0]
+    finalDF = finalDF[['HSHD_NUM'] + [col for col in finalDF.columns if col != 'HSHD_NUM' and col != '_id']]
+
+    #finalDF[["L","AGE_RANGE","MARITAL","INCOME_RANGE","HOMEOWNER","HSHD_COMPOSITION","HH_SIZE","CHILDREN"]] = finalDF[["L","AGE_RANGE","MARITAL","INCOME_RANGE","HOMEOWNER","HSHD_COMPOSITION","HH_SIZE","CHILDREN"]][0].astype(str)
+    #print(finalDF,file=sys.stderr)
 
     ##QUERYING PRODUCTS BY EACH PRODUCT NUMBER FROM ABOVE DF
     productNumberDF = transactionDF[["PRODUCT_NUM"]]
@@ -48,89 +61,99 @@ def standardDatapull(household):
 
     ##FINALLY COMBINE ALL
     finalDF = pd.concat([finalDF, productsDF], axis = 1)
+    finalDF["PURCHASE_"] = pd.to_datetime(finalDF.PURCHASE_, infer_datetime_format=True)
+    finalDF["PURCHASE_"]= finalDF["PURCHASE_"]
+    finalDF = finalDF.sort_values(by=["BASKET_NUM","PURCHASE_","PRODUCT_NUM","DEPARTMENT","COMMODITY"])
+    return finalDF
+
+def getSpendData(): #never gonna be used
+    Collection1 = db.households
+    Collection2 = db.transactions
+    Collection3 = db.products
+
+    #GET CURSORS
+    houseHoldCursor = Collection1.find({"HSHD_NUM":10})
+    transactionCursor = Collection2.find({"HSHD_NUM":10})
+    productCursor = Collection3.find()
+    
+
+    ##TURN CURSOR INTO LIST, THEN INTO DATAFRAME
+    list_houseHoldCursor = list(houseHoldCursor)
+    houseHoldDF = DataFrame(list_houseHoldCursor)
+    list_transactionCursor = list(transactionCursor)
+    transactionDF = DataFrame(list_transactionCursor)
+    list_allProducts = list(productCursor) ##IMPORT ALL PRODUCTS BECAUSE QUERYING THE DB FOR EACH PRODUCT NUMBER WITH SINGLE RETURNS TAKES 2 MINUTES
+    AllProductsDF = DataFrame(list_allProducts)
+
+    ##FORMAT HOUSEHOLD AND TRANSACTION DFS
+    houseHoldDF = houseHoldDF[["HSHD_NUM"]]
+    transactionDF = transactionDF[["BASKET_NUM", "PURCHASE_","SPEND"]]
+    finalDF = pd.concat([houseHoldDF, transactionDF], axis = 1)
+    finalDF["HSHD_NUM"] = finalDF["HSHD_NUM"][0]
+    #print(finalDF,file=sys.stderr)
+    finalDF["PURCHASE_"] = pd.to_datetime(finalDF.PURCHASE_, infer_datetime_format=True)
+    finalDF = finalDF.sort_values(by="PURCHASE_")
+    finalDF = finalDF[["PURCHASE_","SPEND"]]
+    finalDF.index = pd.RangeIndex(len(finalDF.index))
+    finalDF.index = range(len(finalDF.index))
+    finalDF['id'] = finalDF.index
+    finalDF = finalDF.groupby(finalDF['PURCHASE_']).aggregate({'SPEND':'sum'})#.aggregate(aggregation_functions)
+    finalDF['PURCHASE_'] = finalDF.index
+    finalDF['PURCHASE_'] = finalDF['PURCHASE_'].astype(str)
+    spendValues = finalDF['SPEND'].values.tolist()
+    dateValues = finalDF['PURCHASE_'].values.tolist()
+    #print(spendValues,file=sys.stderr)
+    #print(dateValues,file=sys.stderr)
     return finalDF
 
 ## hacky pd dataframe code to mimick the above code. I can make some impovements to it when I add the rest of the colums and sort them proper
 def standardDatapullFiles(household, path, householdsFileName, transactionFileName, productFileName):
-    print(household)
     #header= [ "_id", "HSHD_NUM", "L", "AGE_RANGE", "MARITAL", "INCOME_RANGE", "HOMEOWNER", "HSHD_COMPOSITION", "HH_SIZE", "CHILDREN"]
-    uploads_dir = os.path.join(path, 'uploads\\')
-##################################
-    houseHoldDF = pd.read_csv(uploads_dir + householdsFileName, encoding = 'ISO-8859-1')
-    houseHoldColNames =[]
-    for col in houseHoldDF.columns:
-        houseHoldColNames.append(col)
-    houseHoldDF = houseHoldDF.loc[houseHoldDF[houseHoldColNames[0]] == household]
-    print(houseHoldDF,file=sys.stderr)
-    ##JUST HOUSEHOLD
-#########################################
     
-    transactionDF = pd.read_csv(uploads_dir + transactionFileName, encoding = 'ISO-8859-1')
-    transactionColNames =[]
-    for col in transactionDF.columns:
-        transactionColNames.append(col)
-    #print(transactionColNames,file=sys.stderr)
-    transactionDF = transactionDF.loc[transactionDF[transactionColNames[1]] == household]
-    transactionDF = transactionDF[[transactionColNames[0], transactionColNames[2], transactionColNames[3]]]
-    transactionDF["HSHD_NUM"] = household
-    transactionDF = transactionDF[["HSHD_NUM", transactionColNames[0], transactionColNames[2], transactionColNames[3]]]
-    print(transactionDF,file=sys.stderr)
-    #HSHLD, ["BASKET_NUM", "PURCHASE_","PRODUCT_NUM"]
-##########################################
-    productDF = pd.read_csv(uploads_dir + productFileName, encoding = 'ISO-8859-1')
-    print(productDF,file=sys.stderr)
-    productColNames =[]
-    for col in productDF.columns:
-        productColNames.append(col)
-    productNumberDF = transactionDF[[transactionColNames[3]]]
-    print(productNumberDF,file=sys.stderr)
-    filteredProductDF = pd.DataFrame(columns = productColNames)
-    print(filteredProductDF,file=sys.stderr)
-    for index,row in productNumberDF.iterrows():
-        entry = productDF.loc[productDF[productColNames[0]] == row[productColNames[0]]]
-        filteredProductDF = filteredProductDF.append(entry, ignore_index=True)
-    filteredProductDF = filteredProductDF[[productColNames[1],productColNames[2]]]
-    ##DEPARTMENT, COMMODITY
-    print(filteredProductDF,file=sys.stderr)
-    #transactionDF = transactionDF.rename(columns={'HSHD_NUM': 'HSHD_NUM', transactionColNames[0]: 'BASKET_NUM', transactionColNames[2]: 'PURCHASE', transactionColNames[3]: 'PRODUCT_NUM'})
-    transactionDF.index = pd.RangeIndex(len(filteredProductDF.index))
-    transactionDF.index = range(len(filteredProductDF.index))
+    uploads_dir = os.path.join(path, 'uploads')
 
-    print(transactionDF,file=sys.stderr)
-    finalDF = pd.concat([transactionDF,filteredProductDF], axis = 1)
-    print(finalDF,file=sys.stderr)
-    finalDFColNames = []
-    for col in finalDF.columns:
-        finalDFColNames.append(col)
-    return finalDF,finalDFColNames
-
-
-
-
-    #Collection1 = db.householdsTemp
-    #Collection2 = db.transactionsTemp
-    #Collection3 = db.productsTemp
-    #x = Collection1.delete_many({})
-    #x = Collection2.delete_many({})
-    #x = Collection3.delete_many({})
+    houseHoldDF = pd.read_csv(os.path.join(uploads_dir,householdsFileName), encoding = 'ISO-8859-1')
+    houseHoldDF = houseHoldDF[[col for col in houseHoldDF.columns if col != '_id']]
+    houseHoldDF = houseHoldDF.loc[houseHoldDF[houseHoldDF.columns[0]] == household]
+    houseHoldDF = houseHoldDF.rename(columns={houseHoldDF.columns[0]:"HSHD_NUM",houseHoldDF.columns[1]:"L",houseHoldDF.columns[2]:"AGE_RANGE",houseHoldDF.columns[3]:"MARITAL",houseHoldDF.columns[4]:"INCOME_RANGE",houseHoldDF.columns[5]:"HOMEOWNER",houseHoldDF.columns[6]:"HSHD_COMPOSITION",houseHoldDF.columns[7]:"HH_SIZE",houseHoldDF.columns[8]:"CHILDREN"})
     #print(houseHoldDF,file=sys.stderr)
-    #Collection1.insert_one({"_id":1})
-    
-    #make_json(uploads_dir + householdsFileName, uploads_dir + householdsFileName[:-3] + '.json')
-
-    #csvfile = open(uploads_dir + householdsFileName, 'r')
-    #df = pd.read_csv(uploads_dir + householdsFileName, encoding = 'ISO-8859-1')
-    #data = df.to_json()
-    #print(data,file=sys.stderr)  
-    #header= [ "_id", "HSHD_NUM", "L", "AGE_RANGE", "MARITAL", "INCOME_RANGE", "HOMEOWNER", "HSHD_COMPOSITION", "HH_SIZE", "CHILDREN"]
-    
-        
-
-    #householdsDF = pd.read_csv(StringIO(householdsDF), sep='\s+', error_bad_lines=False)
-    #print(householdsDF,file=sys.stderr)
-    #index = householdsDF["HSHD_NUM"] == household
-    #householdsDF = householdsDF["HSHD_NUM"]
-    #print(householdsDF,file=sys.stderr)
-
-    
-    return 
+    transactionDF = pd.read_csv(os.path.join(uploads_dir,transactionFileName), encoding = 'ISO-8859-1')
+    transactionDF = transactionDF[[col for col in transactionDF.columns if col != '_id']]
+    transactionDF = transactionDF.rename(columns={transactionDF.columns[0]:"BASKET_NUM",transactionDF.columns[1]:"HSHD_NUM",transactionDF.columns[2]:"PURCHASE_",transactionDF.columns[3]:"PRODUCT_NUM",transactionDF.columns[4]:"SPEND",transactionDF.columns[5]:"UNITS",transactionDF.columns[6]:"STORE_R",transactionDF.columns[7]:"WEEK_NUM",transactionDF.columns[8]:"YEAR"})
+    transactionDF = transactionDF.loc[transactionDF[transactionDF.columns[1]] == household]
+    houseHoldDF = houseHoldDF[[col for col in houseHoldDF.columns if col != 'HSHD_NUM']]
+    finalDF = pd.concat([transactionDF, houseHoldDF ], axis = 1)
+    #print(finalDF,file=sys.stderr)
+    #print(finalDF.iloc[0]["L"])
+    finalDF["L"] = finalDF.iloc[0]["L"]
+    finalDF["AGE_RANGE"] = finalDF.iloc[0]["AGE_RANGE"]
+    finalDF["MARITAL"] = finalDF.iloc[0]["MARITAL"]
+    finalDF["INCOME_RANGE"] = finalDF.iloc[0]["INCOME_RANGE"]
+    finalDF["HOMEOWNER"] = finalDF.iloc[0]["HOMEOWNER"]
+    finalDF["HSHD_COMPOSITION"] = finalDF.iloc[0]["HSHD_COMPOSITION"]
+    finalDF["HH_SIZE"] = finalDF.iloc[0]["HH_SIZE"]
+    finalDF["CHILDREN"] = finalDF.iloc[0]["CHILDREN"]
+    finalDF = finalDF.iloc[1: , :]
+    finalDF = finalDF[['HSHD_NUM'] + [col for col in finalDF.columns if col != 'HSHD_NUM' and col != '_id']]
+    #print(finalDF,file=sys.stderr)
+    productDF = pd.read_csv(os.path.join(uploads_dir,productFileName), encoding = 'ISO-8859-1')
+    productDF = productDF.rename(columns={productDF.columns[0]:"PRODUCT_NUM",productDF.columns[1]:"DEPARTMENT",productDF.columns[2]:"COMMODITY",productDF.columns[3]:"BRAND_TY",productDF.columns[4]:"NATURAL_ORGANIC_FLAG"})
+    allProductDF = productDF
+    productDF = productDF.iloc[0:0]
+    productNumberDF = finalDF[["PRODUCT_NUM"]]
+    for index, row in productNumberDF.iterrows():
+        entry = allProductDF.loc[allProductDF["PRODUCT_NUM"] == int(row["PRODUCT_NUM"])]
+        productDF = productDF.append([entry])
+    productDF = productDF[["DEPARTMENT","COMMODITY"]]
+    productDF.index = pd.RangeIndex(len(productDF.index))
+    productDF.index = range(len(productDF.index))
+    finalDF.index = pd.RangeIndex(len(productDF.index))
+    finalDF.index = range(len(productDF.index))
+    #print(productDF,file=sys.stderr)
+    finalDF = pd.concat([productDF, finalDF], axis = 1)
+    finalDF = finalDF[['HSHD_NUM','BASKET_NUM','PURCHASE_','PRODUCT_NUM','DEPARTMENT','COMMODITY'] + [col for col in finalDF.columns if col != 'HSHD_NUM' and col != 'BASKET_NUM'and col != 'PURCHASE_' and col != 'PRODUCT_NUM' and col != 'DEPARTMENT' and col != 'COMMODITY']]
+    #print(finalDF,file=sys.stderr)
+    finalDF["PURCHASE_"] = pd.to_datetime(finalDF.PURCHASE_, infer_datetime_format=True)
+    finalDF["PURCHASE_"] = finalDF["PURCHASE_"]
+    finalDF = finalDF.sort_values(by=["BASKET_NUM","PURCHASE_","PRODUCT_NUM","DEPARTMENT","COMMODITY"])
+    return finalDF
--- app/routes.py
+++ app/routes.py
@@ -27,45 +27,7 @@ def home():
 
 @app.route('/datapullHousehold10')
 def datapullHousehold10():
-    Collection1 = db.households
-    Collection2 = db.transactions
-    Collection3 = db.products
-
-    #GET CURSORS
-    houseHoldCursor = Collection1.find({"HSHD_NUM":10})
-    transactionCursor = Collection2.find({"HSHD_NUM":10})
-    productCursor = Collection3.find()
-    
-
-    ##TURN CURSOR INTO LIST, THEN INTO DATAFRAME
-    list_houseHoldCursor = list(houseHoldCursor)
-    houseHoldDF = DataFrame(list_houseHoldCursor)
-    list_transactionCursor = list(transactionCursor)
-    transactionDF = DataFrame(list_transactionCursor)
-    list_allProducts = list(productCursor) ##IMPORT ALL PRODUCTS BECAUSE QUERYING THE DB FOR EACH PRODUCT NUMBER WITH SINGLE RETURNS TAKES 2 MINUTES
-    AllProductsDF = DataFrame(list_allProducts)
-
-    ##FORMAT HOUSEHOLD AND TRANSACTION DFS
-    houseHoldDF = houseHoldDF[["HSHD_NUM"]]
-    transactionDF = transactionDF[["BASKET_NUM", "PURCHASE_","PRODUCT_NUM"]]
-    finalDF = pd.concat([houseHoldDF, transactionDF], axis = 1)
-    finalDF["HSHD_NUM"] = finalDF["HSHD_NUM"][0]
-
-    ##QUERYING PRODUCTS BY EACH PRODUCT NUMBER FROM ABOVE DF
-    productNumberDF = transactionDF[["PRODUCT_NUM"]]
-    productsDF = DataFrame(columns=["_id","PRODUCT_NUM", "DEPARTMENT", "COMMODITY", "BRAND_TY", "NATURAL_ORGANIC_FLAG"])
-    list_productNumbers = []
-    for index, row in productNumberDF.iterrows():
-        entry = AllProductsDF.loc[AllProductsDF["PRODUCT_NUM"] == int(row["PRODUCT_NUM"])]
-        productsDF = productsDF.append([entry])
-    productsDF = productsDF[["DEPARTMENT", "COMMODITY"]]
-
-    ##MUST REINDEX BECAUSE DUPLICATE INDEXES IN PRODUCTSDF
-    productsDF.index = pd.RangeIndex(len(productsDF.index))
-    productsDF.index = range(len(productsDF.index))
-
-    ##FINALLY COMBINE ALL
-    finalDF = pd.concat([finalDF, productsDF], axis = 1)
+    finalDF = queries.standardDatapull(10)
     return render_template('datapullHousehold10.html', household_df = finalDF)
 
 @app.route('/datapullCustomInput', methods=['GET', 'POST'])
@@ -83,8 +45,11 @@ def datapullCustom():
 
 @app.route('/customerEngagement', methods = ['GET', 'POST'])
 def customerEngagement():
-    # do stuff
-    return render_template("customerEngagement.html")
+    
+    finalDF = queries.getSpendData()
+    #finalDF = finalDF
+    #print(finalDF,file=sys.stderr)
+    return render_template("customerEngagement.html", finalDF = finalDF)
 
 @app.route('/demographicFactorsCustomerEngagement', methods = ['GET', 'POST'])
 def demographicFactorsCustomerEngagement():
@@ -104,20 +69,14 @@ def create():
         households = request.files['myfile1']
         households.save(os.path.join(uploads_dir, secure_filename(households.filename)))
         householdsFileName = secure_filename(households.filename)
-        #householdsDF = pd.read_csv(households)
-        #print(householdsDF, file=sys.stderr)
     if 'myfile2' in request.files:
         transactions = request.files['myfile2']
         transactions.save(os.path.join(uploads_dir, secure_filename(transactions.filename)))
         transactionsFileName = secure_filename(transactions.filename)
-        #transactionsDF = pd.read_csv(transactions)
-        #print(transactionsDF, file=sys.stderr)
     if 'myfile3' in request.files:
         products = request.files['myfile3']
         products.save(os.path.join(uploads_dir, secure_filename(products.filename)))
         productsFileName = secure_filename(products.filename)
-        #productsDF = pd.read_csv(products)
-        #print(productsDF, file=sys.stderr)
     desiredHousehold = request.form['desiredHousehold']
     path = app.instance_path
     if('myfile3' in request.files and 'myfile2' in request.files and 'myfile1' in request.files and desiredHousehold != "" and request.method == 'POST'):
@@ -126,22 +85,13 @@ def create():
 
 @app.route('/datapullCustomFiles', methods=['GET', 'POST'])
 def datapullCustomFiles():
-    print("hi", file=sys.stderr)
     desiredHousehold = request.args.get('desiredHousehold')
     path = request.args.get('path')
     householdsFileName = request.args.get('householdsFileName')
     transactionsFileName = request.args.get('transactionsFileName')
     productsFileName = request.args.get('productsFileName')
-    #householdsDF = request.args.get('householdsDF')
-    #transactionsDF = request.args.get('transactionsDF')
-    #productsDF = request.args.get('productsDF')
-    finalDF, col_names = queries.standardDatapullFiles( int(desiredHousehold), path, householdsFileName, transactionsFileName, productsFileName)
-    ##TODO query function for datapull
-
-    #transactionsDF = request.form.get('transactionsDF')
-    #productsDF = request.form.get('productsDF')
-    #finalDF = ""#queries.standardDatapull(int(desiredHousehold))
-    return render_template('datapullCustomFiles.html',desiredHousehold = desiredHousehold, finalDF = finalDF, col_names = col_names)
+    finalDF = queries.standardDatapullFiles( int(desiredHousehold), path, householdsFileName, transactionsFileName, productsFileName)
+    return render_template('datapullCustomFiles.html',desiredHousehold = desiredHousehold, finalDF = finalDF)
 
 @app.route('/one')
 def one():
